e7c4ed393e8afd887caa4a75c9a36e2ec2900888
Yasmin <yasmin.bunnie@gmail.com>
2022-03-30 18:15:43 +0100

    added sequelize

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("/config");
 
+// Why do we use app?
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// What does this do again?
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Why do we use force: ??
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
