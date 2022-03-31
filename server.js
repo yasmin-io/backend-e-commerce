bfa3906bc36f37c7648f1a3c880645c2a8a82340
Yasmin <yasmin.bunnie@gmail.com>
2022-03-31 01:23:00 +0100

    creating sequelize connection

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const routes = require("./routes");
-const sequelize = require("/config");
+const sequelize = require("./config/connection");
 
-// Why do we use app?
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// What does this do again?
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-// Why do we use force: ??
+// Forces a db to be dropped before trying to create a new one
+// Don't want to rewrite db tables everytime you start the connection
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
