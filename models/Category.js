0ac24fafc4e7cbffb9414eeb9075a2d62d04b291
Yasmin <yasmin.bunnie@gmail.com>
2022-03-31 06:08:24 +0100

    finished category and product models

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
+  // Define fields/columns on model
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
+    // Link to database connection
     sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
     timestamps: false,
+    // Prevent sequelize from renaming the table
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    // Name of the class is what will be set at the model name
+    modelName: "category",
   }
 );
 
+// Export the model
 module.exports = Category;
